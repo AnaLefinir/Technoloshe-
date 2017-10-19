fc13d987221311a83a2127d9aa1f9aa6c336eed7
AnaLefinir <analefinir@gmail.com>
2017-10-18 22:55:43 -0300

    Ejercicio resulto n°1

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Technoloshe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> concursantes = new List<string>();
+            int flag = 1;
+            string concursante;
+            int ganador;
+            Random r = new Random();
+
+            Console.WriteLine("Ingrese -1 cuando haya terminado de cargar a los concursantes");
+
+            while (flag != -1)
+            {
+                Console.WriteLine("Ingrese el nombre completo del concursante");
+                concursante = Console.ReadLine();
+                
+                if (int.TryParse(concursante, out flag) != true)
+                {
+                    concursantes.Add(concursante);
+                }
+
+            }
+
+            if (concursantes.Count > 0)
+            {
+                ganador = r.Next(concursantes.Count);
+                Console.WriteLine("El ganador es " + concursantes[ganador]);
+            }
+            else
+            {
+                Console.WriteLine("Nadie se anoto al concurso");
+            }
+        }
+    }
+}
